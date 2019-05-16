cdb900f60d9f2e3eb02ecd565a753a8cc1d8bf96
kiranbandi <bvenkatkiran@gmail.com>
2019-05-16 02:58:21 -0600

    readwear event tracking

--- utils/jb_run.js
+++ utils/jb_run.js
@@ -12,17 +12,17 @@ var thisPath = process.cwd();
 var jbrowsePath = "./";
 
 // check if jbrowse is a module
-if (fs.pathExistsSync(thisPath+"/node_modules/@gmod/jbrowse/utils")) {
-	jbrowsePath = "./node_modules/@gmod/jbrowse";
+if (fs.pathExistsSync(thisPath + "/node_modules/@gmod/jbrowse/utils")) {
+    jbrowsePath = "./node_modules/@gmod/jbrowse";
 }
 
 // command line options
 var getopt = new getopt([
-    ['p', 'port=NUMBER' , 'listening port'],
+    ['p', 'port=NUMBER', 'listening port'],
     ['s', 'suburi=STRING', 'sub-URI path'],
     ['h', 'help', 'display this help']
-]);              // create Getopt instance
-getopt.bindHelp();     // bind option 'help' to default action
+]); // create Getopt instance
+getopt.bindHelp(); // bind option 'help' to default action
 opt = getopt.parseSystem(); // parse command line
 
 var port = 3000;
@@ -35,23 +35,22 @@ if (typeof setPort !== 'undefined') {
 var app = express();
 
 var dispPort = "";
-if (port !== 80) dispPort = ":"+port;
+if (port !== 80) dispPort = ":" + port;
 
 
-app.use('/'+(opt.options['suburi']||''), express.static(
-    jbrowsePath,
-    {
+app.use('/jbrowse' + (opt.options['suburi'] || ''), express.static(
+    jbrowsePath, {
         // set Content-Encoding: gzip on .jsonz and .gz files
-        setHeaders(res,path,stat) {
-            if( /\.(txt|json|g)z$/.test(path) && ! res.req.headers.range ) {
-                res.setHeader('Content-Encoding','gzip');
+        setHeaders(res, path, stat) {
+            if (/\.(txt|json|g)z$/.test(path) && !res.req.headers.range) {
+                res.setHeader('Content-Encoding', 'gzip');
             }
             res.setHeader('Cache-Control', 'public')
         }
     }
 ));
 
-app.listen(port, function () {
-    console.log('JBrowse is running on port %s',port);
-    console.log('Point your browser to http://localhost%s',dispPort);
+app.listen(port, function() {
+    console.log('JBrowse is running on port %s', port);
+    console.log('Point your browser to http://localhost%s', dispPort);
 });
